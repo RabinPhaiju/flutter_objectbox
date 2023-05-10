bea61b8c4ef50db0e3b735e87fbe668fabe9ef83
Rabin Phaiju <rabinphaiju15@gmail.com>
2023-05-10 08:26:56 +0545

    added input controller dispose

--- lib/screens/forum/add_post.dart
+++ lib/screens/forum/add_post.dart
@@ -18,27 +18,38 @@ class _NewPostState extends State<NewPost> {
   final _replyController = TextEditingController();
   final _relationController = TextEditingController();
 
+  void submitPost(){
+    final enteredUserId = _userIdController.text;
+    final enteredUserName = _userNameController.text;
+    final enteredId = _idController.text;
+    final enteredRelId = _relIdController.text;
+    final enteredTitle = _titleController.text;
+    final enteredReply = _replyController.text;
+    final enteredRelation = _relationController.text;
+
+    if(enteredTitle.isEmpty){return;}
+    // widget only available in stateful
+    // widget is special property that gives access to the context of your Widget
+    widget.addPost(enteredUserId,enteredUserName,enteredId,enteredRelId,enteredTitle,enteredReply,enteredRelation);
+    // close bottom sheet
+    Navigator.of(context).pop();
+  }
+
+  @override
+  void dispose() {
+    _userIdController.dispose();
+    _userNameController.dispose();
+    _idController.dispose();
+    _relIdController.dispose();
+    _titleController.dispose();
+    _replyController.dispose();
+    _relationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-
-    void submitPost(){
-      final enteredUserId = _userIdController.text;
-      final enteredUserName = _userNameController.text;
-      final enteredId = _idController.text;
-      final enteredRelId = _relIdController.text;
-      final enteredTitle = _titleController.text;
-      final enteredReply = _replyController.text;
-      final enteredRelation = _relationController.text;
-
-      if(enteredTitle.isEmpty){return;}
-      // widget only available in stateful
-      // widget is special property that gives access to the context of your Widget
-      widget.addPost(enteredUserId,enteredUserName,enteredId,enteredRelId,enteredTitle,enteredReply,enteredRelation);
-      // close bottom sheet
-      Navigator.of(context).pop();
-    }
-
-    return SingleChildScrollView(
+        return SingleChildScrollView(
       child: Card(
         elevation: 5,
         child: Container(
